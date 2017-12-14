412406827fd3ae2de7e05722512e1ab82caacc4b
Julien <julien.courtial@laposte.net>
2017-12-14 22:51:07 +0100

    adding v6

---
+++ src/jus/poc/prodcons/v6/Consommateur.java
@@ -0,0 +1,65 @@
+package jus.poc.prodcons.v6;
+
+import jus.poc.prodcons.Acteur;
+import jus.poc.prodcons.Aleatoire;
+import jus.poc.prodcons.ControlException;
+import jus.poc.prodcons.Message;
+import jus.poc.prodcons.Observateur;
+import jus.poc.prodcons._Consommateur;
+
+public class Consommateur extends Acteur implements _Consommateur {
+
+	private ProdCons buffer;
+	private int nbmsg;
+	private Aleatoire time;
+	private Observateur ob;
+
+	protected Consommateur(Observateur observateur, int moyenneTempsDeTraitement, int deviationTempsDeTraitement,
+			ProdCons buffer) throws ControlException {
+		super(Acteur.typeConsommateur, observateur, moyenneTempsDeTraitement, deviationTempsDeTraitement);
+
+		this.buffer = buffer;
+		this.nbmsg = 0;
+
+		time = new Aleatoire(moyenneTempsDeTraitement, deviationTempsDeTraitement);
+		ob = observateur;
+		this.setDaemon(true);
+	}
+
+	@Override
+	// Renvoi le nombre de message deja consommé
+	public int nombreDeMessages() {
+		return nbmsg;
+	}
+
+	public void run() {
+		int t = 0;
+
+		Message msg;
+		while (true) {
+			// On attend un certain temps avant de consommé
+			try {
+				t = time.next();
+				sleep(t * 100);
+			} catch (InterruptedException e1) {
+				e1.printStackTrace();
+			}
+
+			try {
+				msg = buffer.get(this);
+				System.out.println(msg);
+				ob.consommationMessage(this, msg, t);
+				nbmsg++;
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} catch (Exception e) {
+
+				e.printStackTrace();
+
+			}
+			Thread.yield();
+		}
+
+	}
+
+}
---
+++ src/jus/poc/prodcons/v6/TestProdCons.java
@@ -0,0 +1,116 @@
+package jus.poc.prodcons.v6;
+
+import java.io.IOException;
+import java.util.InvalidPropertiesFormatException;
+import java.util.Map;
+import java.util.Properties;
+
+import jus.poc.prodcons.Observateur;
+import jus.poc.prodcons.Simulateur;
+
+public class TestProdCons extends Simulateur {
+	protected int nbProd;
+	protected int nbCons;
+	protected int nbBuffer;
+	protected int tempsMoyenProduction;
+	protected int deviationTempsMoyenProduction;
+	protected int tempsMoyenConsommation;
+	protected int deviationTempsMoyenConsommation;
+	protected int nombreMoyenDeProduction;
+	protected int deviationNombreMoyenDeProduction;
+	protected int nombreMoyenNbExemplaire;
+	protected int deviationNombreMoyenNbExemplaire;
+
+	protected Observateur ob;
+
+	public TestProdCons(Observateur observateur) {
+		super(observateur);
+		ob = observateur;
+	}
+
+	protected void run() throws Exception {
+		// le corps de votre programme principal
+
+		// On récupère les variables dans un fichier xml
+		init("jus/poc/prodcons/options/options.xml");
+
+		Consommateur cons[] = new Consommateur[nbCons];
+
+		Producteur prod[] = new Producteur[nbProd];
+
+		ProdCons buffer = new ProdCons(ob, nbBuffer);
+		
+		ob.init(nbProd,nbCons,nbBuffer);
+
+		// On créer et on démarre les consommateurs
+		for (int i = 0; i < nbCons; i++) {
+			cons[i] = new Consommateur(ob, tempsMoyenConsommation, deviationTempsMoyenConsommation, buffer);
+			cons[i].start();
+			ob.newConsommateur(cons[i]);
+		}
+
+		// On créé et on démarre les producteurs
+		for (int i = 0; i < nbProd; i++) {
+			prod[i] = new Producteur(ob, tempsMoyenProduction, deviationTempsMoyenProduction, buffer,
+					nombreMoyenDeProduction, deviationNombreMoyenDeProduction);
+			prod[i].start();
+			ob.newProducteur(prod[i]);
+		}
+
+		// On boucle tant que tout ce qui doit être produit n'a pas été
+		// consommé
+		while (!(sum_prod(prod, nbProd) == sum_cons(cons, nbCons))) {
+
+		}
+
+		System.out.println("Fini");
+	}
+
+	// Calcule la somme des messages à produire par les producteurs
+	public int sum_prod(Producteur p[], int taille) {
+		int somme = 0;
+		for (int i = 0; i < taille; i++) {
+			somme += p[i].nombreDeMessages();
+		}
+		return somme;
+	}
+
+	// Calcule la somme des messages déjà consommé par les consommateurs
+	public int sum_cons(Consommateur c[], int taille) {
+		int somme = 0;
+		for (int i = 0; i < taille; i++) {
+			somme += c[i].nombreDeMessages();
+		}
+		return somme;
+	}
+
+	public static void main(String[] args) {
+		new TestProdCons(new Observateur()).start();
+	}
+
+	/**
+	 * Retreave the parameters of the application.
+	 * 
+	 * @param file
+	 *            the final name of the file containing the options.
+	 * @throws IOException
+	 * @throws InvalidPropertiesFormatException
+	 * @throws SecurityException
+	 * @throws NoSuchFieldException
+	 * @throws IllegalAccessException
+	 * @throws IllegalArgumentException
+	 */
+	protected void init(String file) throws InvalidPropertiesFormatException, IOException, IllegalArgumentException,
+			IllegalAccessException, NoSuchFieldException, SecurityException {
+		Properties properties = new Properties();
+		properties.loadFromXML(ClassLoader.getSystemResourceAsStream(file));
+		String key;
+		int value;
+		Class<?> thisOne = getClass();
+		for (Map.Entry<Object, Object> entry : properties.entrySet()) {
+			key = (String) entry.getKey();
+			value = Integer.parseInt((String) entry.getValue());
+			thisOne.getDeclaredField(key).set(this, value);
+		}
+	}
+}
