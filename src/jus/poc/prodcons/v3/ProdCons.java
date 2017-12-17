51629d08738d69ee3dd65555df7f0e3c80339548
Julien <julien.courtial@laposte.net>
2017-12-17 17:10:30 +0100

    adding doc for v3

--- src/jus/poc/prodcons/v3/ProdCons.java
+++ src/jus/poc/prodcons/v3/ProdCons.java
@@ -9,18 +9,50 @@ import jus.poc.prodcons.Tampon;
 import jus.poc.prodcons._Consommateur;
 import jus.poc.prodcons._Producteur;
 
+/**
+ * 
+ * @author AUBERT Vincent et COURTIAL Julien
+ *
+ */
 public class ProdCons implements Tampon {
 
+	/**
+	 * L'observateur du professeur
+	 */
 	private Observateur ob;
+	/**
+	 * La capacité du buffer
+	 */
 	private int capacity;
+	/**
+	 * Le buffer
+	 */
+	private ArrayBlockingQueue<Message> buffer;
+	/**
+	 * Le semaphore représentant la condition de consommation.
+	 */
+	private Semaphore nonVide = new Semaphore(0);
+	/**
+	 * Le semaphore représentant la condition de production.
+	 */
+	private Semaphore nonPlein;
+	/**
+	 * Le semaphore de protection du partage de données pour la methode put
+	 */
+	private Semaphore mutexIn = new Semaphore(1);
+	/**
+	 * Le semaphore de protection du partage de données pour la méthode get.
+	 */
+	private Semaphore mutexOut = new Semaphore(1);
 
-	private ArrayBlockingQueue<Message> buffer; // FIFO
-	private Semaphore nonVide = new Semaphore(0); // Condition de consommation
-
-	private Semaphore nonPlein; // Condition de production
-	private Semaphore mutexIn = new Semaphore(1); // Protection pour le partage
-	private Semaphore mutexOut = new Semaphore(1); // des données
-
+	/**
+	 * Le constructeur du buffer
+	 * 
+	 * @param ob
+	 *            L'observateur du professeur
+	 * @param capacity
+	 *            La capacité du buffer
+	 */
 	public ProdCons(Observateur ob, int capacity) {
 		this.ob = ob;
 		this.capacity = capacity;
@@ -29,14 +61,25 @@ public class ProdCons implements Tampon {
 		nonPlein = new Semaphore(this.capacity);
 	}
 
-	@Override
-	// Retourne le nombre de message déjà ajouté dans le tampon
+	/**
+	 * Le nombre de message dans le buffer
+	 * 
+	 * @return le nombre de message déjà ajouté dans le buffer
+	 */
 	public int enAttente() {
 		return buffer.size();
 	}
 
-	@Override
-	// Recupère un message dans le tampon
+	/**
+	 * Bloque tant qu'il n'est pas possible de retirer puis retire un message de
+	 * tête du buffer
+	 * 
+	 * @param arg0
+	 *            Le consommateur du message
+	 * @return Le message en tête du buffer
+	 * @throws Exception
+	 * @throws InterruptedException
+	 */
 	public Message get(_Consommateur arg0) throws Exception, InterruptedException {
 		try {
 			nonVide.acquire();
@@ -58,8 +101,17 @@ public class ProdCons implements Tampon {
 		return message;
 	}
 
-	@Override
-	// Ajoute un message dans le tampon
+	/**
+	 * Bloque tant qu'il n'est pas possible d'ajouter et ajoute un message en
+	 * fin de buffer
+	 * 
+	 * @param arg0
+	 *            Le producteur du message
+	 * @param arg1
+	 *            Le message produit
+	 * @throws Exception
+	 * @throws InterruptedException
+	 */
 	public void put(_Producteur arg0, Message arg1) throws Exception, InterruptedException {
 
 		try {
@@ -78,8 +130,11 @@ public class ProdCons implements Tampon {
 
 	}
 
-	@Override
-	// Capacité maximale du tampon
+	/**
+	 * La taille du buffer
+	 * 
+	 * @return La capacité du buffer
+	 */
 	public int taille() {
 		return capacity;
 	}
--- src/jus/poc/prodcons/v3/Producteur.java
+++ src/jus/poc/prodcons/v3/Producteur.java
@@ -6,45 +6,86 @@ import jus.poc.prodcons.ControlException;
 import jus.poc.prodcons.Observateur;
 import jus.poc.prodcons._Producteur;
 
+/**
+ * 
+ * @author AUBERT Vincent et COURTIAL Julien
+ *
+ */
 public class Producteur extends Acteur implements _Producteur {
+	/**
+	 * Le buffer associé au programme
+	 */
 	private ProdCons buffer;
+	/**
+	 * Le nombre de messages que le producteur doit produire
+	 */
 	private int nbmsg;
+	/**
+	 * Variable aléatoire de temps
+	 */
 	private Aleatoire time;
+	/**
+	 * L'observateur du professeur
+	 */
 	private Observateur ob;
 
+	/**
+	 * 
+	 * Constructeur d'un producteur
+	 * 
+	 * @param observateur
+	 *            L'observateur du professeur
+	 * @param moyenneTempsDeTraitement
+	 *            La moyenne de temps de production
+	 * @param deviationTempsDeTraitement
+	 *            La déviation de la moyenne de temps de production
+	 * @param buffer
+	 *            Le buffer associé au programme
+	 * @param nombreMoyenDeProduction
+	 *            Le nombre moyen de messages à produire
+	 * @param deviationNombreMoyenDeProduction
+	 *            La déviation du nombre moyen de messages à produire
+	 * @throws ControlException
+	 */
 	protected Producteur(Observateur observateur, int moyenneTempsDeTraitement, int deviationTempsDeTraitement,
 			ProdCons buffer, int nombreMoyenDeProduction, int deviationNombreMoyenDeProduction)
 			throws ControlException {
 		super(Acteur.typeProducteur, observateur, moyenneTempsDeTraitement, deviationTempsDeTraitement);
 		this.buffer = buffer;
-		
+
 		nbmsg = Aleatoire.valeur(nombreMoyenDeProduction, deviationNombreMoyenDeProduction);
 
 		time = new Aleatoire(moyenneTempsDeTraitement, deviationTempsDeTraitement);
-		ob=observateur;
+		ob = observateur;
 	}
 
-	@Override
-	// Renvoi le nombre de message que doit produire le producteur
+	/**
+	 * Renvoie le nombre de message que doit produire le producteur
+	 * 
+	 * @return Le nombre de message à produire
+	 */
 	public int nombreDeMessages() {
 		return nbmsg;
 	}
 
+	/**
+	 * 
+	 */
 	public void run() {
-		int t=0;
+		int t = 0;
 		MessageX msg;
 		for (int i = 0; i < nbmsg; i++) {
 
 			// On attend un certain temps avant de produire un message
 			try {
-				t=time.next();
+				t = time.next();
 				sleep(t);
 			} catch (InterruptedException e1) {
 				e1.printStackTrace();
 			}
 
 			try {
-				msg= new MessageX("Bonjour"+i);
+				msg = new MessageX("Bonjour" + i);
 				ob.productionMessage(this, msg, t);
 				buffer.put(this, msg);
 			} catch (InterruptedException e) {
@@ -52,7 +93,7 @@ public class Producteur extends Acteur implements _Producteur {
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			
+
 			Thread.yield();
 		}
 
